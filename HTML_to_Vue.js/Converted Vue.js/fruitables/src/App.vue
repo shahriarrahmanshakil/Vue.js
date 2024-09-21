50b7021ec1ab62aeb87145ca29ccfd1117dd9350
Shahriar Rahman Shakil <shahriarrahmanshakil@gmail.com>
2024-09-21 12:51:36 +0600

    footer added

--- HTML_to_Vue.js/Converted Vue.js/fruitables/src/App.vue
+++ HTML_to_Vue.js/Converted Vue.js/fruitables/src/App.vue
@@ -1,13 +1,16 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
 import Topber from './components/layouts/Topber.vue';
+import Navber from './components/layouts/Navber.vue';
+import Footer from './components/layouts/Footer.vue';
 
 </script>
 
 <template>
   <Topber />
-
+  <Navber />
   <RouterView />
+  <Footer> </Footer>
 </template>
 
 <style scoped>
